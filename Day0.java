50ed6f74e28ab8324ac7ccac4f7bc1f11c6fa82e
ADITYA_MISHRA <90920322+Adityamishra45@users.noreply.github.com>
2022-12-20 21:31:33 +0530

    Add files via upload

---
+++ Day0.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class Day0 {
+	public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in); 
+	String inputString = scan.nextLine(); 
+    System.out.println("Hello, World.");
+    System.out.println(inputString);
+	
+		scan.close(); 
+      
+	}
+}
+
+
